32b47e4fe96842d52d6b888aaf75ab6951eed036
zikri29 <70123043+zikri29@users.noreply.github.com>
2020-08-25 09:07:46 +0700

    Add files via upload

---
+++ nitip.php
@@ -0,0 +1,31 @@
+<?php
+ 
+class mobil{
+ 
+    var $warna;
+    var $merek;
+    var $ukuran;
+ 
+   function maju(){
+ 
+   }
+ 
+   function berhenti(){
+    
+   }
+ 
+}
+ 
+?>
+
+<?php
+
+
+class mobil{
+  
+
+}
+
+$mobil = new mobil();
+
+?>
